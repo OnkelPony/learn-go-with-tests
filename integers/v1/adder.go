36838237531720afa86541448fcdc2674201e5a2
Jiří Martínek <zwo@centrum.cz>
2019-12-14 00:19:35 +0100

    Final edit

--- integers/v1/adder.go
+++ integers/v1/adder.go
@@ -1,6 +1,7 @@
 package integers
 
 // Add takes two integers and returns the sum of them.
-func Add(x, y int) int {
-	return x + y
+func Add(i int, i2 int) (result int) {
+	result = i + i2
+	return result
 }
