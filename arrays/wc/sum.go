37d129cf8f8f5dce8275348c857a8dcc62efeb82
Jiří Martínek <zwo@centrum.cz>
2019-12-15 01:46:49 +0100

    In the end of exercises

--- arrays/wc/sum.go
+++ arrays/wc/sum.go
@@ -7,3 +7,25 @@ func Sum(numbers []int) int {
 	}
 	return result
 }
+
+func SumAll(slices ...[]int) []int {
+	slicesLength := len(slices)
+	result := make([]int, slicesLength)
+	for i := 0; i < slicesLength; i++ {
+		result[i] = Sum(slices[i])
+	}
+	return result
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var result []int
+	for _, slice := range slices {
+		if len(slice) > 0 {
+			tail := slice[1:]
+			result = append(result, Sum(tail))
+		} else {
+			result = append(result, 0)
+		}
+	}
+	return result
+}
--- arrays/wc/sum_test.go
+++ arrays/wc/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("Collection of variable size", func(t *testing.T) {
@@ -14,4 +17,49 @@ func TestSum(t *testing.T) {
 		}
 	})
 
+	t.Run("Slice of slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9, 10})
+		want := []int{3, 19}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v want %v", got, want)
+		}
+	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(got []int, want []int, t *testing.T) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sum of some slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{1, 2}, []int{0, 9, 10})
+		want := []int{2, 19}
+
+		checkSums(got, want, t)
+	})
+
+	t.Run("safe sum of empty slices", func(t *testing.T) {
+
+		got := SumAllTails([]int{}, []int{6, 6, 6})
+		want := []int{0, 12}
+
+		checkSums(got, want, t)
+	})
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2}, []int{0, 9, 10})
+	}
+}
+
+func BenchmarkSumAllTails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2}, []int{0, 9, 10})
+	}
 }
