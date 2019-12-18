409df9d8272c6a5183034f50469cf80728c30d42
Jiří Martínek <zwo@centrum.cz>
2019-12-18 22:15:54 +0100

    Exercise finished

--- di/v1/di.go
+++ di/v1/di.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 // Greet sends a personalised greeting to writer
+
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
 func main() {
-	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
